3a7dcdb0bf2fe2309ad4b87400b49b02ee4793af
Mateo <mateo.guerrero.or@gmail.com>
2026-06-01 23:04:04 -0500

    feat: ejercicio 1 - sistema de biblioteca con menu

---
+++ ejercicio-01-biblioteca/BibliotecaApp/Models/Publicacion.cs
@@ -0,0 +1,23 @@
+namespace BibliotecaApp
+{
+  public class Publicacion
+  {
+    public string Titulo{ get; set; }
+    public int Anio { get; set; }
+    protected string ISBN { get; set; }
+    public double Costo { get; set; }
+
+    public Publicacion(string titulo, int anio, string isbn, double costo)
+    {
+      Titulo = titulo;
+      Anio = anio;
+      ISBN = isbn;
+      Costo = costo;
+    }
+
+    public virtual void MostrarInformacion()
+    {
+      Console.WriteLine($"El isbn es [{ISBN}] el titulo es [{Titulo}] el año de publicacion es [{Anio}] y el costo es de [{Costo}]");
+    }
+  }
+}
--- ejercicio-01-biblioteca/BibliotecaApp/Program.cs
+++ ejercicio-01-biblioteca/BibliotecaApp/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BibliotecaApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool continuar = true;
+      int op;
+      Biblioteca biblioteca = new Biblioteca("Biblioteca central", "Calle 123");
+
+      do
+      {
+        Console.WriteLine("Este es el menu de la biblioteca");
+        Console.WriteLine("Para crear una nueva publicacion preciona 1");
+        Console.WriteLine("Para mostrar las publicaciones preciona 2");
+        Console.WriteLine("Para salir preciona 3");
+
+        op = Convert.ToInt32(Console.ReadLine());
+        switch (op)
+        {
+          case 1:
+            Console.WriteLine("Para crear una nueva revista preciona 1");
+            Console.WriteLine("Para crear un nuevo libro preciona 2");
+            op = Convert.ToInt32(Console.ReadLine());
+            if (op == 1)
+            {
+              Revista revista = new Revista(15, 23, "Comica", "Chespirito", 2026, "afagf");
+              biblioteca.AgregarPublicacion(revista);
+            }else if(op == 2)
+            {
+              Libro libro = new Libro("Mateo Guerrero", 23, "Comica", "Chespirito", 2026, "afagf");
+              biblioteca.AgregarPublicacion(libro);
+            }
+            break;
+          case 2:
+            biblioteca.MostrarPublicaciones();
+            break;
+          case 3:
+            Console.WriteLine("Saliendo del programa");
+            continuar = false;
+            break;
+          default:
+            Console.WriteLine("Esa opcion no esta en la lista");
+            break;
+        }
+      } while (continuar == true);
+    }
+  }
+}
